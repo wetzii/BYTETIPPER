8f183daf07744a948c7680a3b90fff2e5fe67b45
wetzii <240062@o365.htl-leoben.at>
2026-03-20 16:46:10 +0100

    code now real

--- BYTETIPPER.py
+++ BYTETIPPER.py
@@ -4,11 +4,18 @@ import random
 
 def givRandTime() -> int:
     return random.randint(1,3)
+while(True): 
+    pyautogui.press('s')
+    time.sleep(givRandTime())
+    pyautogui.press('y')
+    time.sleep(givRandTime())
+    pyautogui.press('s')
+    time.sleep(givRandTime())
+    pyautogui.press('o')
+    time.sleep(givRandTime())
+    pyautogui.press('enter')
 
-pyautogui.press('s')
-pyautogui.press('y')
-pyautogui.press('s')
-pyautogui.press('o')
-pyautogui.press('enter')
-time.sleep(1)
+    for i in range(1,60):
+        time.sleep(1)
+        print(f"Wartezeit : {60-i} Sekunden")
 
